bb555f314f019db2a53f7de315bb79668f467419
freelancernasimofficial <freelancernasim@gmail.com>
2024-01-19 05:44:31 +0600

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import * as url from "url";
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const pathToExtension = path.join(__dirname, "2captcha");
-// puppeteer usage as normal
+
 puppeteer
   .launch({
     targetFilter: (target) => !!target.url(),
@@ -25,38 +25,124 @@ puppeteer
   })
   .then(async (browser) => {
     const page = (await browser.pages())[0];
-    await page.setDefaultTimeout(300000);
     await page.setBypassCSP(true);
     await page.authenticate({
       username: "uv3ythl4tgh9uu8",
       password: "m3dkgmvpwc5yq9y",
     });
 
-    await page.goto("https://visa.vfsglobal.com/npl/en/ltp/login");
-    await page.locator("input#mat-input-0").fill("winbazilive@gmail.com");
-    await page.locator("input#mat-input-1").fill("Mak223166@");
-    await page.locator("form button").click();
-    await page.waitForNavigation({ waitUntil: "networkidle2" });
-    const newBookingButton = await page.$(
-      "button.mat-focus-indicator.btn.mat-btn-lg.btn-block.btn-brand-orange.mat-raised-button.mat-button-base",
-    );
-    await newBookingButton.evaluate((b) => b.click());
-    setInterval(async () => {
-      await page.evaluate(() => {
-        if (
-          !document.querySelector(
-            "mat-select#mat-select-4 div.mat-select-value>span>span.mat-select-min-line",
-          )?.innerText
-        ) {
-          document.querySelector("mat-select#mat-select-4")?.click();
-          document.querySelector("mat-option#mat-option-2")?.click();
+    await page.goto("https://visa.vfsglobal.com/npl/en/ltp/login", {
+      waitUntil: "networkidle2",
+    });
+
+    const intervalCount = setInterval(async () => {
+      if (
+        (await page.url()) === "https://visa.vfsglobal.com/npl/en/ltp/login"
+      ) {
+        console.log("process working for", await page.url());
+        const emailBox = await page.$("input#mat-input-0");
+        const pwdBox = await page.$("input#mat-input-1");
+        if (emailBox && pwdBox) {
+          const isEmailEntered = await emailBox.evaluate(
+            (el) => el?.value?.length,
+          );
+          const isPwdEntered = await emailBox.evaluate(
+            (el) => el?.value?.length,
+          );
+          if (!isEmailEntered) {
+            await emailBox.type("winbazilive@gmail.com");
+            console.log("email entered");
+          }
+          if (!isPwdEntered) {
+            await pwdBox.type("Mak223166@");
+            console.log("password entered");
+          }
         }
-      });
 
+        const submitButton = await page.$("form button");
+        if (submitButton) {
+          await submitButton.click();
+        }
+
+        const recaptcha = await page.$("form iframe[title='reCAPTCHA']");
+        if (recaptcha) {
+          console.log("recaptcha found reloading");
+          clearInterval(intervalCount);
+          await page.reload();
+        }
+      }
+
+      if (
+        (await page.url()) === "https://visa.vfsglobal.com/npl/en/ltp/dashboard"
+      ) {
+        console.log("process working for", await page.url());
+        const gotoAppBtn = await page.$("button.mat-btn-lg");
+        if (gotoAppBtn) {
+          await gotoAppBtn.evaluate((btn) => btn.click());
+        }
+      }
+
+      if (
+        (await page.url()) ===
+        "https://visa.vfsglobal.com/npl/en/ltp/application-detail"
+      ) {
+        console.log("process working for", await page.url());
+
+        //start changing application category
+
+        //get application category select option tag
+        const selectApplicationCategoryTag = await page.$("#mat-select-4");
+
+        //get selected span tag
+        const selectedOptionSpanTag = await page.$(
+          "#mat-select-4>div>div>span>span",
+        );
+        //Select option number 2 if not already selected
+        if (!selectedOptionSpanTag) {
+          if (selectApplicationCategoryTag) {
+            //click options tag
+            await selectApplicationCategoryTag.click();
+          }
+
+          //get the option 2
+          const appCatValue2 = await page.$("#mat-option-2");
+          if (appCatValue2) {
+            //click the option 2
+            await appCatValue2.click();
+            console.log("option 2 selected");
+          }
+        }
+
+        //if already selected
+        if (selectedOptionSpanTag) {
+          const selectedText = await selectedOptionSpanTag.evaluate(
+            (el) => el.innerText,
+          );
+          if (selectedText === "Lithuania Temporary Residence Permit") {
+            await selectApplicationCategoryTag.click();
+            //get the option 1
+            const appCatValue1 = await page.$("#mat-option-1");
+            if (appCatValue1) {
+              //click the option 1
+              await appCatValue1.click();
+              console.log("option 1 selected");
+            }
+          }
+          if (selectedText === "Lithuania National D Visa") {
+            await selectApplicationCategoryTag.click();
+            //get the option 1
+            const appCatValue2 = await page.$("#mat-option-2");
+            if (appCatValue2) {
+              //click the option 1
+              await appCatValue2.click();
+              console.log("option 2 selected");
+            }
+          }
+        }
+
+        //end application changing category
+      }
       console.log("interval: ", Date.now());
-    }, 1000);
-
-    // await page.screenshot({ path: Date.now() + ".jpg", fullPage: true });
-    // await browser.close();
-    console.log("all done");
+      // await page.screenshot({ path: "page.jpg", fullPage: true });
+    }, 100);
   });
